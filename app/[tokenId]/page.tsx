400bb94993fcfe84add0f2065f4ab5f3f3920fdc
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-28 17:20:17 -0300

    I reorganize routing to be simpler for cre8ors use-case.

--- app/[tokenId]/page.tsx
+++ app/[tokenId]/page.tsx
@@ -6,17 +6,14 @@ import { isNil } from "lodash";
 import { getAccount, getAccountStatus, getLensNfts, getNfts } from "@/lib/utils";
 import { rpcClient } from "@/lib/clients";
 import { TbLogo } from "@/components/icon";
-import { useGetApprovals, useNft } from "@/lib/hooks";
+import { useNft } from "@/lib/hooks";
 import { TbaOwnedNft } from "@/lib/types";
-import { getAddress } from "viem";
 import { TokenDetail } from "./TokenDetail";
 import { HAS_CUSTOM_IMPLEMENTATION } from "@/lib/constants";
 
 interface TokenParams {
   params: {
     tokenId: string;
-    contractAddress: string;
-    chainId: string;
   };
   searchParams: {
     apiEndpoint: string;
@@ -27,9 +24,13 @@ export default function Token({ params, searchParams }: TokenParams) {
   const [imagesLoaded, setImagesLoaded] = useState(false);
   const [nfts, setNfts] = useState<TbaOwnedNft[]>([]);
   const [lensNfts, setLensNfts] = useState<TbaOwnedNft[]>([]);
-  const { tokenId, contractAddress, chainId } = params;
+  const contractAddress = process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS as string;
+  const chainId = process.env.NEXT_PUBLIC_TESTNET ? 5 : 1;
+  const { tokenId } = params;
   const [showTokenDetail, setShowTokenDetail] = useState(false);
-  const chainIdNumber = parseInt(chainId);
+  console.log("SWEETS contractAddress", contractAddress);
+  console.log("SWEETS chainId", chainId);
+  console.log("SWEETS tokenId", tokenId);
 
   const {
     data: nftImages,
@@ -37,9 +38,9 @@ export default function Token({ params, searchParams }: TokenParams) {
     loading: nftMetadataLoading,
   } = useNft({
     tokenId: parseInt(tokenId as string),
-    contractAddress: params.contractAddress as `0x${string}`,
+    contractAddress: contractAddress as `0x${string}`,
     hasCustomImplementation: HAS_CUSTOM_IMPLEMENTATION,
-    chainId: chainIdNumber,
+    chainId,
   });
 
   useEffect(() => {
@@ -65,7 +66,7 @@ export default function Token({ params, searchParams }: TokenParams) {
 
   // Fetch nft's TBA
   const { data: account } = useSWR(tokenId ? `/account/${tokenId}` : null, async () => {
-    const result = await getAccount(Number(tokenId), contractAddress, chainIdNumber);
+    const result = await getAccount(Number(tokenId), contractAddress, chainId);
     return result.data;
   });
 
@@ -82,7 +83,7 @@ export default function Token({ params, searchParams }: TokenParams) {
       return false;
     }
 
-    const { data, error } = await getAccountStatus(chainIdNumber, account!);
+    const { data, error } = await getAccountStatus(chainId, account!);
 
     return data ?? false;
   });
@@ -90,10 +91,7 @@ export default function Token({ params, searchParams }: TokenParams) {
   // fetch nfts inside TBA
   useEffect(() => {
     async function fetchNfts(account: string) {
-      const [data, lensData] = await Promise.all([
-        getNfts(chainIdNumber, account),
-        getLensNfts(account),
-      ]);
+      const [data, lensData] = await Promise.all([getNfts(chainId, account), getLensNfts(account)]);
       if (data) {
         setNfts(data);
       }
@@ -105,34 +103,20 @@ export default function Token({ params, searchParams }: TokenParams) {
     if (account) {
       fetchNfts(account);
     }
-  }, [account, accountBytecode, chainIdNumber]);
+  }, [account, accountBytecode, chainId]);
 
   const [tokens, setTokens] = useState<TbaOwnedNft[]>([]);
-  const allNfts = [...nfts, ...lensNfts];
-
-  const { data: approvalData } = useGetApprovals(allNfts, account, chainIdNumber);
 
   useEffect(() => {
     if (nfts !== undefined && nfts.length) {
-      nfts.map((token) => {
-        const foundApproval = approvalData?.find((item) => {
-          const contract = item.contract.address;
-          const tokenId = item.tokenId;
-          const hasApprovals = item.hasApprovals;
-          const matchedAddress = getAddress(contract) === getAddress(token.contract.address);
-          const matchedTokenId = String(tokenId) && String(token.tokenId);
-          if (matchedAddress && matchedTokenId && hasApprovals) {
-            return true;
-          }
-        });
-        token.hasApprovals = foundApproval?.hasApprovals || false;
-      });
       setTokens(nfts);
       if (lensNfts) {
         setTokens([...nfts, ...lensNfts]);
       }
     }
-  }, [nfts, approvalData, lensNfts]);
+  }, [nfts, lensNfts]);
+
+  console.log("SWEETS nftImages", nftImages);
 
   return (
     <div className="h-screen w-screen bg-slate-100">
@@ -142,11 +126,11 @@ export default function Token({ params, searchParams }: TokenParams) {
             <TokenDetail
               isOpen={showTokenDetail}
               handleOpenClose={setShowTokenDetail}
-              approvalTokensCount={approvalData?.filter((item) => item.hasApprovals).length}
+              approvalTokensCount={0}
               account={account}
               tokens={tokens}
               title={nftMetadata.title}
-              chainId={chainIdNumber}
+              chainId={chainId}
             />
           )}
           <div className="max-h-1080[px] relative h-full w-full max-w-[1080px]">
